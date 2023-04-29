4ef7adcca56bac0d99ed500a1817beae5fbda24a
Erik Russell <erikrussells@gmail.com>
2023-04-29 14:05:50 -0400

    still working

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,9 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 import Clock from "../clock/Clock";
 import "./nav.css";
 
 function Nav() {
-  // const [isDark, setIsDark] = useState(true);
+  const [isDark, setIsDark] = useState(true);
 
   return (
     <div className="nav-body-div">
@@ -12,15 +12,15 @@ function Nav() {
           <Clock />
         </div>
         <div className="nav-text">info</div>
-        {/* {isDark ? ( */}
+        {isDark ? (
           <button onClick={() => setIsDark(false)} className="nav-btn">
             Dark
           </button>
-        {/* ) : ( */}
+         ) : ( 
           <button onClick={() => setIsDark(true)} className="nav-btn-dark">
             Light
           </button>
-        {/* )} */}
+         )}
       </div>
     </div>
   );
