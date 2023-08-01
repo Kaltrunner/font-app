385d088a819f3db9e76088fb006c06e1e9f23bce
Erik Russell <erikrussells@gmail.com>
2023-08-01 16:41:37 -0400

    updated mouse

--- src/components/mouse/Mouse.jsx
+++ src/components/mouse/Mouse.jsx
@@ -7,15 +7,15 @@ function Mouse() {
       <AnimatedCursor
         id="mouse"
         innerSize={0}
-        outerSize={300}
+        outerSize={30}
         color="208,83,255"
         outerAlpha={0.5}
         innerScale={0}
-        outerScale={0.5}
-        trailingSpeed={20}
+        outerScale={1}
+        trailingSpeed={0}
         filter={true}
         outerStyle={{
-          filter: "blur(30px)",
+          filter: "blur(2px)",
         }}
       />
     </>
